7eed07b637fa86254cec83413acb92e04aafef34
hetvi11110 <hetvi.patel.585112@gmail.com>
2024-04-24 21:54:55 -0400

    build: unit test with HTTP POST

--- controllers/todo.contoller.js
+++ controllers/todo.contoller.js
@@ -1,12 +1,10 @@
 const TodoModel = require('../model/todo.model');
-
 /* 
 * https://nodejs.org/docs/v20.12.1/api/modules.html#exports
 */
 exports.createTodo = (req, res, next) => {
-    console.log(req);
-    console.log(res);
-    console.log(next);
     //https://mongoosejs.com/docs/api/model.html#Model.create()
-    TodoModel.create();
+    const createModel = TodoModel.create(req.body);
+    //https://expressjs.com/en/4x/api.html#res.status
+    res.status(201).json(createModel);
 };
--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,12 +1,13 @@
-
 /*      
 * Modules: CommonJS modules: require(id): https://nodejs.org/docs/latest/api/modules.html#requireid
 * In Node.js, each file is treated as a separate module. For example, consider a file named
 */
 const TodoController    = require('../../controllers/todo.contoller');
 const TodoModel         = require('../../model/todo.model');
+
 // https://www.npmjs.com/package/node-mocks-http
 const httpMocks         = require('node-mocks-http');
+const newTodo           = require('../mock-data/new-todo.json')
 
 /* 
 * https://mongoosejs.com/docs/models.html#constructing-documents
@@ -18,24 +19,42 @@ const httpMocks         = require('node-mocks-http');
 */
 TodoModel.create = jest.fn();
 
+let req,res,next;
+/* 
+* Globals: beforeEach(fn, timeout): https://jestjs.io/docs/api#beforeeachfn-timeout
+*/
+beforeEach(() => {
+    req = httpMocks.createResponse();
+    res = httpMocks.createResponse();
+    next = null;
+});
+
 /* 
 * https://jestjs.io/docs/cli#--configpath
 *
 * Introduction: More Resources: Api References: Globals: describe(name, fn): https://jestjs.io/docs/api#describename-fn
 * creates a block that groups together several related tests.
 * 
-* Globals: test(name, fn, timeout): https://jestjs.io/docs/api#testname-fn-timeout
-* Also under the alias: it(name, fn, timeout)
+* Globals: test(name, fn, timeout): Also under the alias: it(name, fn, timeout): https://jestjs.io/docs/api#testname-fn-timeout
 * All you need in a test file is the test method which runs a test.
 *
 * Api References: Expect: expect(value): https://jestjs.io/docs/expect#expectvalue
 * The expect function is used every time you want to test a value.
 *
 * Expect: Matchers: .toBe(value): https://jestjs.io/docs/expect#tobevalue
+* Expect: Matchers: .toHaveBeenCalled(): Also under the alias: .toBeCalled(): https://jestjs.io/docs/expect#tohavebeencalled
+* Expect: Matchers: .toHaveBeenCalledWith(arg1, arg2, ...): Also under the alias: .toBeCalledWith(): https://jestjs.io/docs/expect#tohavebeencalledwitharg1-arg2-
+* Expect: Matchers: .toBeTruthy(): https://jestjs.io/docs/expect#tobetruthy
+* Expect: Matchers: .toStrictEqual(value): https://jestjs.io/docs/expect#tostrictequalvalue
 * Use .toBe to compare primitive values or to check referential identity of object instances.
+* Mock Functions: mockFn.mockReturnValue(value): https://jestjs.io/docs/mock-function-api#mockfnmockreturnvaluevalue
 */
 
 describe('TodoController.createTodo', () => {
+
+    beforeEach(() => {
+        req.body = newTodo;
+    });
 
     test('should have a createTodo function', () => {
         // The typeof operator returns a string indicating the type of the operand's value.
@@ -44,12 +63,22 @@ describe('TodoController.createTodo', () => {
     });
 
     test('should call TodoModel.create',() => {
-        let req,res,next;
-        req = httpMocks.createResponse();
-        res = httpMocks.createResponse();
-        next = null;
         //called function from todo.contoller.js
-        TodoController.createTodo(req, res, next);
-        expect(TodoModel.create).toBeCalled();
+        TodoController.createTodo(req, res, next);       
+        expect(TodoModel.create).toHaveBeenCalledWith(newTodo);
     });
+
+    test('sould return 201 responde code', () => {
+        TodoController.createTodo(req, res, next); 
+        expect(res.statusCode).toBe(201);
+        //https://www.npmjs.com/package/node-mocks-http#usage
+        expect(res._isEndCalled()).toBeTruthy();
+    });
+
+    test('sould return json body in respone',() => {
+        TodoModel.create.mockReturnValue(newTodo);
+        TodoController.createTodo(req, res, next);
+        expect(res._getJSONData()).toStrictEqual(newTodo);
+    });
+
 });
